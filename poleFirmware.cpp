026df0da26f9a1d25034001a15832a577e5a98f1
gab0r <jgabriel.orozco@udea.edu.co>
2023-04-04 09:35:13 -0500

    The operating system is successfully included.

--- poleFirmware.cpp
+++ poleFirmware.cpp
@@ -1,12 +1,28 @@
+#include <FreeRTOS.h>
+#include <task.h>
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+
+void led_task(void*)
+{   
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        vTaskDelay(1000);
+        gpio_put(LED_PIN, 0);
+        vTaskDelay(1000);
+    }
+}
 
 int main()
 {
     stdio_init_all();
 
-    puts("Hello, world!");
+    xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    vTaskStartScheduler();
 
-    return 0;
+    while(1){};
 }
