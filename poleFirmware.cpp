ce830544544cb1be6db478c2ba097f43ecb054ae
gab0r <jgabriel.orozco@udea.edu.co>
2023-04-04 11:23:57 -0500

    Periodic trigger task is created

--- poleFirmware.cpp
+++ poleFirmware.cpp
@@ -2,27 +2,34 @@
 #include <task.h>
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "timers.h"
 
 
-void led_task(void*)
-{   
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        vTaskDelay(1000);
-        gpio_put(LED_PIN, 0);
-        vTaskDelay(1000);
-    }
-}
+#define PERIOD_TRIGGER_TASK 5000UL          //Period to launch the next measurement stage
+
+/* Bits to trigger tasks */
+
+
+/* Tasks */
+void periodicTrigger(void *pvParameters);   //Periodic trigger for the measurement stage
+
 
 int main()
 {
     stdio_init_all();
 
-    xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    xTaskCreate(periodicTrigger, "PeriodicTrigger", 1000, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while(1){};
+}
+
+void periodicTrigger(void *pvParameters){
+    const TickType_t xDelayTrigger = pdMS_TO_TICKS(PERIOD_TRIGGER_TASK), xDontBLock = 0;
+    while (true)
+    {
+        vTaskDelay(xDelayTrigger);
+        printf("<---- LAUNCHING MEASUREMENT STAGE ---->\r\n");
+    }
+    
 }
